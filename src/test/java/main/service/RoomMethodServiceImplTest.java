01e00e68eab4d5348388879555c8b94fd5381649
NatalyaPonomareva <55796385+NatalyaPonomareva@users.noreply.github.com>
2023-05-15 01:08:59 +0300

    Update RoomMethodServiceImplTest.java

--- src/test/java/main/service/RoomMethodServiceImplTest.java
+++ src/test/java/main/service/RoomMethodServiceImplTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
